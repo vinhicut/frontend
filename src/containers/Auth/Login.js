fda719879dc5f36eb6602efe8697e22222b6f6cb
Vinh Nguyen <nguyenvinhvan90@gmail.com>
2022-10-22 11:26:30 +0700

    create login

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+import * as actions from "../../store/actions";
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+import { Label } from 'reactstrap';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (  //render ra 1 khối thôi, có thể dùng nhiều khối nhưng phải bọc lại
+            <div className='login-background'>
+                <div className='login-container'>
+                    <div className='login-content'>
+                        <div className='col-12 text-login'>Login</div>
+                        <div className="col-12 frorm-group login-input">
+                            <Label>Username</Label>
+                            <input type='text' className='form-control' placeholder='Enter your username' />
+                        </div>
+                        <div className="col-12 frorm-group login-input">
+                            <Label>Password</Label>
+                            <input type='password' className='form-control' placeholder='Enter your password' />
+                        </div>
+                        <div className='col-12 '>
+                            <button className='btn-login'>Login</button>
+                        </div>
+                        <div className='col-12'>
+                            <span className='forgot-password'>Forgot your password?</span>
+                        </div>
+                        <div className='col-12 text-center mt-3'>
+                            <span className='text-other'>or Login with</span>
+
+                        </div>
+                        <div className='col-12 social-login'>
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
