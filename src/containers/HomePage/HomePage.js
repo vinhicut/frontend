b8581a64dea393473aa891d4dcaccf0547ac1d8e
Vinh Nguyen <nguyenvinhvan90@gmail.com>
2022-10-30 14:40:12 +0700

    tich hop thu vien React Slider-Carousel

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
+import Specialty from './Section/Specialty';
 class HomePage extends Component {
 
     render() {
@@ -10,6 +11,10 @@ class HomePage extends Component {
         return (
             <div>
                 <HomeHeader />
+                <Specialty />
+                <div style={{ height: '300px' }}>
+
+                </div>
             </div>
 
         );
